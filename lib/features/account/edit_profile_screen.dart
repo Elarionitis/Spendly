51a18d7f479767bca80ee37b2798850711e93d58
Suhan Ramani <suhanramani09@gmail.com>
2026-04-05 00:01:03 +0530

    Feature: Add edit profile screen with user metadata support

--- lib/features/account/edit_profile_screen.dart
+++ lib/features/account/edit_profile_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import '../../core/models/app_user.dart';
 import '../../core/repositories/repository_providers.dart';
 import '../../core/theme/app_theme.dart';
 import '../../core/widgets/shared_widgets.dart';
@@ -11,7 +10,12 @@ import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 import '../../core/services/cloudinary_service.dart';
 
-// ... rest of the imports ...
+class EditProfileScreen extends ConsumerStatefulWidget {
+  const EditProfileScreen({super.key});
+
+  @override
+  ConsumerState<EditProfileScreen> createState() => _EditProfileScreenState();
+}
 
 class _EditProfileScreenState extends ConsumerState<EditProfileScreen> {
   final _formKey = GlobalKey<FormState>();
