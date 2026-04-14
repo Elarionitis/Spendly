60f53f6cfa9b0f87ee0eee2782f387897d88ff7d
Suhan Ramani <suhanramani09@gmail.com>
2026-04-14 23:13:14 +0530

    fix: correct balance update after settlement

--- lib/features/settlements/balance_selection_screen.dart
+++ lib/features/settlements/balance_selection_screen.dart
@@ -179,7 +179,7 @@ class _SettleOption extends StatelessWidget {
           ),
           boxShadow: [
             BoxShadow(
-              color: Colors.black.withOpacity(0.03),
+              color: Colors.black.withValues(alpha: 0.03),
               blurRadius: 8,
               offset: const Offset(0, 2),
             ),
